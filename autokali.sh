c416d3e722bbdf6f65b44bed142c93089b4a07d0
Den-html <71197583+Den-html@users.noreply.github.com>
2020-11-21 11:52:19 +0700

    Add files via upload

---
+++ autokali.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+clear
+echo "{           Auto kali                  }"
+echo "{  press 1 for install main reposites  }"
+read numb
+if [ $numb = "1" ]
+then
+	apt-get update
+	apt-get upgrade
+	git clone https://github.com/htr-tech/zphisher.git
+	git clone https://github.com/epsylon/ufonet.git
+	git clone https://github.com/derv82/wifite2.git
+	git clone https://github.com/Bitwise-01/Passwords
+	git clone https://github.com/aircrack-ng/mdk4
+	git clone https://github.com/XCHADXFAQ77X/XERXES
+fi
